0d399d441fb1e2404d60fcd86e8aa464a9ea7338
yiheng98 <huangyh1998@foxmail.com>
2024-11-21 17:45:29 +0800

    feat: add new field in the item

--- article/article/items.py
+++ article/article/items.py
@@ -8,6 +8,7 @@ import scrapy
 
 class ArticleItem(scrapy.Item):
     url = scrapy.Field()
+    web_name = scrapy.Field()
     download_html = scrapy.Field()
     output_dir = scrapy.Field()
     pass
