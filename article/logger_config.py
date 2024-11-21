e257496d29ce83f70903b4c6b68e630420725536
yiheng98 <huangyh1998@foxmail.com>
2024-11-21 14:32:18 +0800

    refactor: change the entrance

---
+++ article/logger_config.py
@@ -0,0 +1,39 @@
+from loguru import logger
+import os
+
+
+def setup_spider_logger(log_file_folder: str, log_type: str, log_file_name: str):
+    log_file_path = os.path.join(log_file_folder, log_type, log_file_name)
+    os.makedirs(log_file_folder, exist_ok=True)
+    
+    # 创建默认配置的日志
+    my_logger = logger.bind(name='Article')
+    logger.add(
+        log_file_path,
+        rotation="1 day",
+        format="{time:YYYY-MM-DD at HH:mm:ss} {level} {message}",
+        level="DEBUG",
+        mode='a',
+        filter=lambda record: record["extra"].get("name") == "Article"
+    )
+    my_logger.level('HIGHLIGHT', no=38, color="<yellow>")
+    return my_logger
+
+
+spider_logger = setup_spider_logger('log', 'article', 'article_{time:YYYY-MM-DD}.log')
+
+def setup_controller_logger(log_file_folder: str, log_file_name:str):
+    log_file_path = os.path.join(log_file_folder, log_file_name)
+    os.makedirs(log_file_folder, exist_ok=True)
+    
+    # 创建总日志
+    my_logger = logger.bind(name='Controller')
+    logger.add(log_file_path,
+               format="{time:YYYY-MM-DD at HH:mm:ss} {level} {message}",
+               level='DEBUG',
+               mode='a',
+               filter=lambda record: record["extra"].get("name") == "Controller"
+               )
+    return my_logger
+
+controller_logger= setup_controller_logger('log', 'controller.log')
--- article/run_spider.py
+++ article/run_spider.py
@@ -1,10 +1,18 @@
 import os
-
+import pymysql
 from scrapy.crawler import CrawlerProcess
 from article.spiders.main_crawl import MainCrawlSpider
 from scrapy.utils.project import get_project_settings
 import multiprocessing
-import sys
+import argparse
+from datetime import datetime
+from logger_config import controller_logger as logger
+import traceback
+
+parser = argparse.ArgumentParser(description='web crawl')
+parser.add_argument('-web', nargs='*', type=str, help='A list of websites')
+parser.add_argument('-m', choices=['add','full'], required=True)
+args = parser.parse_args()
 
 def crawl_website(param):
     settings = get_project_settings()
@@ -13,28 +21,41 @@ def crawl_website(param):
     process.start()
 
 if __name__ == '__main__':
-    full_mode = True
+    logger.info(f"启动对网站的爬取")
+    full_mode = args.m == 'full'
+    web_specific = args.web # 可选参数 指定个别web进行爬取 默认全部爬取
     folder_path = './spider_config'
     target_websites_dedicate = [file.split('.')[0] for file in os.listdir(folder_path)]
-    if len(sys.argv) == 1 or (len(sys.argv) == 2 and (sys.argv[-1] == 'full' or sys.argv[-1] == 'add')):
-        target_websites = target_websites_dedicate
-        if len(sys.argv) > 1:
-            full_mode = sys.argv[-1] == 'full'
+    if web_specific is None:
+        params = [{'target_websites': website, 'full_mode': full_mode} 
+                  for website in target_websites_dedicate]
     else:
-        if sys.argv[-1] == 'full' or sys.argv[-1] == 'add':
-            target_websites = sys.argv[1:-1]
-            full_mode = sys.argv[-1] == 'full'
-        else:
-            target_websites = sys.argv[1:]
-        for website in target_websites:
-            if website not in target_websites_dedicate:
-                print(f'"{website}" is not define')
-                exit(0)
-    # 构建每个网站的独立参数字典
-    params = [{'target_websites': website, 'full_mode': full_mode} for website in target_websites]
+        params = [{'target_websites': website, 'full_mode': full_mode} 
+                  for website in target_websites_dedicate
+                  if web_specific is not None and website in web_specific]
 
-    with multiprocessing.Pool(processes=len(target_websites)) as pool:
+    with multiprocessing.Pool(processes=len(params)) as pool:
         pool.map(crawl_website, params)
-
-
-
+    
+    connection = pymysql.connect(
+        host='10.176.56.233',
+        port=3310,
+        user='root',
+        password='114514',
+        database='intelligenceSource'
+    )
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    query = """
+        SELECT * FROM source WHERE crawl_date = "{}" and source = "{}";
+    """
+    try:
+        with connection.cursor() as cursor:
+            for param in params:
+                cursor.execute(query.format(current_date, param['target_websites']))
+                result = cursor.fetchall()
+                logger.info(f"本次对 {param['target_websites']} 爬取新增条目 {len(result)}")
+    except Exception as e:
+        logger.error(traceback.format_exc())
+    finally:
+        if connection:
+            connection.close()
